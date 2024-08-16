8de59ace088e19cca66a338dc1a89af79c868ea0
ComposeVenture <composeventures@gmail.com>
2024-08-16 07:16:24 +0300

    Create WebsiteTest.js

---
+++ WebsiteTest.js
@@ -0,0 +1,104 @@
+{
+  "websites": [
+    {
+      "name": "Google",
+      "url": "https://www.google.com",
+      "is_real": true
+    },
+    {
+      "name": "Facebok",
+      "url": "https://www.facebok.com",
+      "is_real": false
+    },
+    {
+      "name": "Amazon",
+      "url": "https://www.amazon.com",
+      "is_real": true
+    },
+    {
+      "name": "Twittr",
+      "url": "https://www.twittr.com",
+      "is_real": false
+    },
+    {
+      "name": "LinkedIn",
+      "url": "https://www.linkedin.com",
+      "is_real": true
+    },
+    {
+      "name": "Reddit",
+      "url": "https://www.reddit.com",
+      "is_real": true
+    },
+    {
+      "name": "Gooogle",
+      "url": "https://www.gooogle.com",
+      "is_real": false
+    },
+    {
+      "name": "YouTube",
+      "url": "https://www.youtube.com",
+      "is_real": true
+    },
+    {
+      "name": "Microsof",
+      "url": "https://www.microsof.com",
+      "is_real": false
+    },
+    {
+      "name": "Yahoo",
+      "url": "https://www.yahoo.com",
+      "is_real": true
+    },
+    {
+      "name": "Instagram",
+      "url": "https://www.instagram.com",
+      "is_real": true
+    },
+    {
+      "name": "WhatsApp",
+      "url": "https://www.whatsapp.com",
+      "is_real": true
+    },
+    {
+      "name": "Airbnnb",
+      "url": "https://www.airbnnb.com",
+      "is_real": false
+    },
+    {
+      "name": "Netflix",
+      "url": "https://www.netflix.com",
+      "is_real": true
+    },
+    {
+      "name": "Spotfy",
+      "url": "https://www.spotfy.com",
+      "is_real": false
+    },
+    {
+      "name": "GitHub",
+      "url": "https://www.github.com",
+      "is_real": true
+    },
+    {
+      "name": "Twittter",
+      "url": "https://www.twittter.com",
+      "is_real": false
+    },
+    {
+      "name": "Pinterest",
+      "url": "https://www.pinterest.com",
+      "is_real": true
+    },
+    {
+      "name": "Alibabba",
+      "url": "https://www.alibabba.com",
+      "is_real": false
+    },
+    {
+      "name": "Quora",
+      "url": "https://www.quora.com",
+      "is_real": true
+    }
+  ]
+}
